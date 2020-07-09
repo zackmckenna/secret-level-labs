58246d5a6d3f44feeb1c6686ac81eb4f2976d2b4
Zack McKenna <zackmckenna@gmail.com>
2020-07-09 12:58:29 -0400

    added tailwind

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Secret Level Labs</h1>
+      <p>Find me in ./web/src/pages/HomePage/HomePage.js</p>
+    </div>
+  )
+}
+
+export default HomePage
